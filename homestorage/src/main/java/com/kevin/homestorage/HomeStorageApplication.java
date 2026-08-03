c8a21f57aa5e23c0928d41061d924f3b38067af6
kogacoder619 <kogacoder@gmail.com>
2026-08-03 00:50:44 -0500

    more adjustments
    will fix later

---
+++ homestorage/src/main/java/com/kevin/homestorage/HomeStorageApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.homestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeStorageApplication.class, args);
+	}
+
+}
